912da137cbf9991696404e7db13233bc2417fd6b
siddharthaSailsLearning <gcplearning.2025.sre@gmail.com>
2025-10-22 14:11:53 +0530

    new trigger 1

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -10,7 +10,7 @@ public class Hello extends HttpServlet {
 
    public void init() throws ServletException {
       // Do required initialization
-      message = "trigger working";
+      message = "trigger working 1";
    }
 
    public void doGet(HttpServletRequest request, HttpServletResponse response)
