1d097ffac198e0dcaeede60927fe1f304f4ff605
siddharthaSailsLearning <gcplearning.2025.sre@gmail.com>
2025-10-22 12:26:12 +0530

    ngrok

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -10,7 +10,7 @@ public class Hello extends HttpServlet {
 
    public void init() throws ServletException {
       // Do required initialization
-      message = "Welcome Jenkins automation..!";
+      message = "stoppped ngork";
    }
 
    public void doGet(HttpServletRequest request, HttpServletResponse response)
